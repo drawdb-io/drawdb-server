98ef86726fc5734846efb64c9568383ce574901b
dott1le <jdhsbhmob@gmail.com>
2023-08-16 04:43:51 +0300

    Handle messages

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello")
+const io = require("socket.io")(5000, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("send-message", (message) => {
+    socket.broadcast.emit("recieve-message", message);
+  });
+});
